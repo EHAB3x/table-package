c217464f2fa389a6c8da31776af83852a2e9b594
EHAB3x <ehabmah6969@gmail.com>
2025-05-28 18:31:48 +0300

    feat: implement CustomTableComponent with pagination and search functionality

---
+++ src/app/components/custom-table/custom-table.component.ts
@@ -0,0 +1,87 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { TableOptions } from '../../core/types/TableType';
+import { debounceTime } from 'rxjs';
+
+@Component({
+  selector: 'app-custom-table',
+  imports: [RouterLink, CommonModule, ReactiveFormsModule],
+  templateUrl: './custom-table.component.html',
+  styleUrl: './custom-table.component.scss'
+})
+export class CustomTableComponent implements OnInit, OnChanges {
+  @Input() type: string = '';
+  @Input() tableData: TableOptions[] = [];
+
+  tableRawData: any[][] = [];
+  originalTableData: TableOptions[] = [];
+  columns: string[] = [];
+  isempty: boolean = true;
+  searchControl = new FormControl('');
+
+  currentPage !: number;
+
+  constructor(
+    private pagination : PaginationService
+  ){
+
+  }
+
+  ngOnInit(): void {
+    this.searchControl.valueChanges.pipe(
+      debounceTime(300)
+    ).subscribe({
+      next: (searchTerm) => {
+        if (!searchTerm) {
+          this.restoreOriginalData();
+        } else {
+          this.filterData(searchTerm);
+        }
+      },
+    });
+  }
+
+  ngOnChanges(): void {
+    if (this.tableData.length > 0) {
+      this.originalTableData = [...this.tableData];
+      this.initializeTableData();
+      this.isempty = false;
+    }else{
+      this.isempty = true;
+    }
+  }
+
+  private initializeTableData(): void {
+    this.columns = Object.keys(this.tableData[0]);
+    this.tableRawData = this.tableData.map(item => Object.values(item)).slice((this.pagination.getCurrentPage() - 1) * 10, this.pagination.getCurrentPage() * 10);
+  }
+
+  private restoreOriginalData(): void {
+    this.tableData = [...this.originalTableData];
+    this.initializeTableData();
+  }
+
+  private filterData(filterText: string): void {
+    const searchTerm = filterText.toLowerCase();
+
+    const filteredData = this.originalTableData.filter(item => {
+      return Object.values(item).some(value =>
+        String(value).toLowerCase().includes(searchTerm)
+      );
+    });
+
+    this.tableData = filteredData;
+    this.updateRawData(filteredData);
+  }
+
+  private updateRawData(data: TableOptions[]): void {
+    this.tableRawData = data.map(item => Object.values(item));
+  }
+
+  // onPagChange(page : number){
+  //   this.initialPage = page;
+  //   this.tableRawData = this.tableData.map(item => Object.values(item)).slice((this.initialPage - 1) * 10, this.initialPage * 10);
+  // }
+}
