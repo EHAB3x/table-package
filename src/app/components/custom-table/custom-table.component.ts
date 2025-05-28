28604df245458d628d86fcad24f638424d29d5f1
EHAB3x <ehabmah6969@gmail.com>
2025-05-28 19:28:44 +0300

    feat: enhance CustomTableComponent with pagination support and data filtering

--- src/app/components/custom-table/custom-table.component.ts
+++ src/app/components/custom-table/custom-table.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, OnDestroy } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { TableOptions } from '../../core/types/TableType';
@@ -13,7 +13,7 @@ import { PaginationComponent } from "../pagination/pagination.component";
   templateUrl: './custom-table.component.html',
   styleUrl: './custom-table.component.scss'
 })
-export class CustomTableComponent implements OnInit, OnChanges {
+export class CustomTableComponent implements OnInit, OnChanges, OnDestroy {
   @Input() type: string = '';
   @Input() tableData: TableOptions[] = [];
 
@@ -24,6 +24,8 @@ export class CustomTableComponent implements OnInit, OnChanges {
   searchControl = new FormControl('');
 
   currentPage !: number;
+  filteredTableData: TableOptions[] = [];
+  private pageSubscription: any;
 
   constructor(
     private pagination : PaginationService
@@ -43,11 +45,24 @@ export class CustomTableComponent implements OnInit, OnChanges {
         }
       },
     });
+    this.pageSubscription = this.pagination.currentPage.subscribe((page) => {
+      this.currentPage = page;
+      this.initializeTableData();
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.pageSubscription) {
+      this.pageSubscription.unsubscribe();
+    }
   }
 
   ngOnChanges(): void {
     if (this.tableData.length > 0) {
       this.originalTableData = [...this.tableData];
+      this.filteredTableData = [...this.tableData];
+      this.currentPage = 1;
+      this.pagination.setCurrentPage(1);
       this.initializeTableData();
       this.isempty = false;
     }else{
@@ -56,34 +71,30 @@ export class CustomTableComponent implements OnInit, OnChanges {
   }
 
   private initializeTableData(): void {
-    this.columns = Object.keys(this.tableData[0]);
-    this.tableRawData = this.tableData.map(item => Object.values(item)).slice((this.pagination.getCurrentPage() - 1) * 10, this.pagination.getCurrentPage() * 10);
+    if (this.filteredTableData.length > 0) {
+      this.columns = Object.keys(this.filteredTableData[0]);
+    }
+    const start = ((this.currentPage || 1) - 1) * 10;
+    const end = start + 10;
+    this.tableRawData = this.filteredTableData.map(item => Object.values(item)).slice(start, end);
   }
 
   private restoreOriginalData(): void {
-    this.tableData = [...this.originalTableData];
+    this.filteredTableData = [...this.originalTableData];
+    this.currentPage = 1;
+    this.pagination.setCurrentPage(1);
     this.initializeTableData();
   }
 
   private filterData(filterText: string): void {
     const searchTerm = filterText.toLowerCase();
-
-    const filteredData = this.originalTableData.filter(item => {
+    this.filteredTableData = this.originalTableData.filter(item => {
       return Object.values(item).some(value =>
         String(value).toLowerCase().includes(searchTerm)
       );
     });
-
-    this.tableData = filteredData;
-    this.updateRawData(filteredData);
+    this.currentPage = 1;
+    this.pagination.setCurrentPage(1);
+    this.initializeTableData();
   }
-
-  private updateRawData(data: TableOptions[]): void {
-    this.tableRawData = data.map(item => Object.values(item));
-  }
-
-  // onPagChange(page : number){
-  //   this.initialPage = page;
-  //   this.tableRawData = this.tableData.map(item => Object.values(item)).slice((this.initialPage - 1) * 10, this.initialPage * 10);
-  // }
 }
